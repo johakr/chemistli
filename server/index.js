9cadd1a8a6e74d223b0a70ba1ca7a62a10439e7d
Johannes Kronmüller <Johannes.Kronmueller@gmail.com>
2017-02-26 14:58:11 +0100

    change server port

--- server/index.js
+++ server/index.js
@@ -68,6 +68,6 @@ app.get('/q/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(3006, () => {
+  console.log('Example app listening on port 3006!');
 });
